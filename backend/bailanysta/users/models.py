5ad70898bc05d0ff70277de4fa6ccb7143bc98b3
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2025-04-26 00:51:33 +0600

    models for user|profile and posts

---
+++ backend/bailanysta/users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+	# profile_picture = models.ImageField(upload_to='profile_pictures/', default='default.jpg')
+	bio = models.TextField(blank=True)
+	short_bio = models.CharField(max_length=50, blank=True)
+	followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+
+	def __str__(self):
+		return f"{self.user.username} Profile"
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return f"Post by {self.user.username} at {self.created_at}"
