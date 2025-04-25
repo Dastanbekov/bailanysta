1074864a213574860d40a364e72b4e74f329471d
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2025-04-26 01:03:12 +0600

    comments model

--- backend/bailanysta/users/models.py
+++ backend/bailanysta/users/models.py
@@ -19,4 +19,12 @@ class Post(models.Model):
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
-        return f"Post by {self.user.username} at {self.created_at}"
+	    return f"Post by {self.user.username} at {self.created_at}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
