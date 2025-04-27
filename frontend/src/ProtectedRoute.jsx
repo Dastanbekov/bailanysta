83e63cee2345195d551cfc851d87c9b785897976
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2025-04-27 10:24:55 +0600

    api

---
+++ frontend/src/ProtectedRoute.jsx
@@ -0,0 +1,37 @@
+import {Navigate} from "react-router-dom"
+import {jwtDecode} from "jwt-decode"
+import api from '../api'
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "./constants"
+import { useState } from "react"
+
+function ProtectedRoute({children}){
+	const [isAuthorized, setIsAuthorized] = useState(null)
+	
+	const refreshToken = async()=>{
+
+	}
+
+	const auth = async() => {
+		const token = localStorage.getItem(ACCESS_TOKEN)
+		if (!token) {
+			setIsAuthorized(false)
+			return 
+		}
+		const decoded = jwtDecode(token)
+		const tokenExpiration = decoded.exp
+		const now = Data.now() / 1000
+
+		if (tokenExpiration < now){
+			await refreshToken()
+		} else {
+			setIsAuthorized(true)
+		}
+	}
+
+	if (isAuthorized == null){
+		return <div>Loading...</div>
+	}
+	return isAuthorized ? childer : <Navigate to='/login'/>
+}
+
+export default ProtectedRoute
