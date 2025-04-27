e276ee264c631afc7bd31af2ba15c3eb598699d4
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2025-04-27 10:25:33 +0600

    ltl changes of back

--- backend/bailanysta/users/urls.py
+++ backend/bailanysta/users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # path('users/', include(router.urls)),
 	path('reg/', views.CreateUserView.as_view(), name = 'user-create'),
     path('posts/', views.PostListCreate.as_view(), name ='post-list-create'),
-	path('post-delete', views.PostDelete.as_view(), name = 'delete-post'),
+	path('post-delete/<int:pk>', views.PostDelete.as_view(), name = 'delete-post'),
 	path('post/<int:pk>', views.PostRetrieve.as_view(), name = 'post-retr'),
 ]
